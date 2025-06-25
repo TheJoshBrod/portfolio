0a9fa3c330d7ae19d2a0ce9e0efaede7729c5da8
joshbrod <joshbrod@umich.edu>
2025-06-24 22:36:32 -0400

    added custom favicon, fixed global styling (including background to be grid (new) vs dotted (old))

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@
 import Header from "@/components/homepage/page";
 import ExperienceSection from "@/components/experience/page";
 import AboutMeSection from "@/components/aboutme/page";
-import CodeBubble from "@/components/CodeBubble";
 
 export default function Home() {
+
   return (
     <>
       <Header/>
       <AboutMeSection/>
       <ExperienceSection/>
+      <div className="h-500"></div>
     </>
   );
 }
