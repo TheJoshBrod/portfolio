2a5351d62bd09b51e1800d80fdff94958a2acc27
joshbrod <joshbrod@umich.edu>
2025-06-24 22:39:27 -0400

    fixed formatting of home page: fixed photo shape, text color, added highlighted header color change, fixed padding so content is centered

--- src/components/homepage/page.tsx
+++ src/components/homepage/page.tsx
@@ -2,13 +2,13 @@ export default function Header() {
   return (
       <>
         {/* Intro Section */}
-        <div data-name="header-intro" id="home" className="h-[95vh] flex flex-col justify-between bg-[#565190] text-[#7d7fa6]">
-          <div className="flex justify-center items-center h-[100%] pb-40">
-            <div className="flex flex-col w-200">
-                  <h1 className="text-8xl drop-shadow-lg">Hey,</h1>
-                  <div className="flex"><h1 className="text-8xl drop-shadow-lg">I'm&nbsp;</h1> <h1 className="text-8xl drop-shadow-lg text-[#37345d]">Josh!</h1></div>
-                  <p className="text-3xl mt-2 mb-6">Incoming CS MSE student at the University of Michigan 〽️</p>
-                  <p className="text-xl">I am a software engineer interested in AI/ML development</p>
+        <div data-name="header-intro" id="home" className="h-[94vh] flex flex-col bg-[rgb(86,81,144)] text-[#c5bfff]">
+          <div className="flex justify-center items-center h-[100%]">
+            <div className="flex flex-col w-150">
+                  <h1 className="text-7xl drop-shadow-lg">Hey,</h1>
+                  <div className="flex"><h1 className="text-7xl drop-shadow-md">I'm&nbsp;</h1> <h1 className="text-7xl font-medium drop-shadow-lg text-[#ede374]">Josh!</h1></div>
+                  <p className="text-3xl mt-2 mb-2 drop-shadow-md">CS MSE @ University of Michigan 〽️</p>
+                  <p className="text-xl drop-shadow-md">Software engineer interested in AI/ML development</p>
                   <div className="flex mt-3 justify-between w-60">
                     <div className="social_media">
                       <img  src="contact_logo/github-color.png"></img>
@@ -21,17 +21,17 @@ export default function Header() {
                     </div>
                   </div>
             </div>
-            <img src="profile_pic.png" className='ml-40 h-200 m-10 drop-shadow-lg rounded-2xl'></img>
+            <img src="image2.png" className='h-[40vh] m-10 border-7 border-[#2e2d4f] drop-shadow-lg rounded-full'></img>
           </div>
         </div>
 
         {/* Navigation Bar */}
-        <div data-name="header-navigation" className="h-[5vh] flex justify-center space-x-16  bg-[#37345d] p-4  text-lg sticky top-0 z-50 text-[#7d7fa6]">  
-            <a href="#home"><p>home</p></a>
-            <a href="#aboutme"><p>about me</p></a>
-            <a href="#experience"><p>experience</p></a>
-            <a href="#section1"><p>portfolio</p></a>
-            <a href="#section1"><p>contact</p></a>
+        <div data-name="header-navigation" className="h-[6vh] flex justify-center items-center space-x-16  bg-[#2e2c4e] p-[1vh]  text-lg sticky top-0 z-50 drop-shadow-sm text-[#7d7fa6]">  
+            <a href="#home" className="navbar-section"><p>home</p></a>
+            <a href="#aboutme" className="navbar-section"><p>about me</p></a>
+            <a href="#experience" className="navbar-section"><p>experience</p></a>
+            <a href="#section1" className="navbar-section"><p>portfolio</p></a>
+            <a href="#section1" className="navbar-section"><p>contact</p></a>
           </div>
       </>
   );
