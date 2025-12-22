e041feb25fb36f528a290f4d9abc44e8b229fe09
joshbrod <joshbrod@umich.edu>
2025-12-22 14:46:15 -0500

    fixed favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦞</text></svg>"/>
+        <link
+          rel="icon"
+          href="data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext y='.9em' font-size='90'%3E🦞%3C/text%3E%3C/svg%3E" />
       </head>
       <body>
         {children}
